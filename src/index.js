067d5b2349ee66d3b9857df1825c6ced92102d2b
Young Eon Park <youngeonpark@Youngs-MacBook-Air.local>
2026-03-25 22:58:51 -0400

    change routing in index

--- src/index.js
+++ src/index.js
@@ -1,59 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route, Outlet } from 'react-router-dom';
 import './index.css';
-import Home from './Pages/Home'
+import Home from './Pages/Home';
 import Education from './Pages/Education';
-import Projects from './Pages/Projects'
-import WorkExperience from './Pages/WorkExperience'
+import Projects from './Pages/Projects';
+import WorkExperience from './Pages/WorkExperience';
 import LandingPage from './Pages/LandingPage';
-import Header from './Layout/Header'
+import Header from './Layout/Header';
 import RoomElement from './Layout/RoomElement';
 import reportWebVitals from './reportWebVitals';
 
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route element={<Header />}>
-//           <Route path="/education" element={<Education />} />
-//           <Route path="/projects" element={<Projects />} />
-//           <Route path="/work-experience" element={<WorkExperience />} />
-//         </Route>
-//       </Routes>
-//       {/* <RoomElement /> */}
-//     </BrowserRouter>
-//   );
-//  }
+function MainLayout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <RoomElement />
+    </>
+  );
+}
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/*"element={<MainLayout />} />
-      </Routes>
-    </BrowserRouter>
-  );
- }
- 
- 
- const MainLayout = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route element={<Header />}>
+        <Route element={<MainLayout />}>
+          <Route path="/home" element={<Home />} />
           <Route path="/education" element={<Education />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/work-experience" element={<WorkExperience />} />
         </Route>
       </Routes>
-      <RoomElement />
-    </>
+    </HashRouter>
   );
- };
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -62,7 +44,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
